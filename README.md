d10842fc8fa33e3d2a1a6583c03e5137c364d6fa
Pedro Vilela <pedro@Pedros-MacBook-Air.local>
2019-10-21 00:07:12 -0300

    project folder

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # TMP
 Monitoramento de carga de caminhão
+
+# TMP
+
+Projeto da Graduação de Sistemas de Controle e Automação para Monitoramento de carga de caminhão
+
+## Habilita/Desabilita a abertura da porta do baú através de um SW utilizando um broker MQTT
+
+truckWeight: truck weight in kg
+alarmStatus: 0 - Alarm OFF ; 1 - Alarm ON
+
+### JSON template:
+
+```json
+{
+    "truckWeight" : 565,
+    "alarmStatus  : 0
+}
+```
+
+## Recebe notificações sobre status de violação do sistema e peso atual do caminhão
+
+doorCommand: 0 - unlock the door ; 1 - lock the door
+truckWeight: truck weight in kg
+alarmCtrl: 0 - does nothing ; 1 - deactivate the alarm
+### JSON template:
+
+```json
+{
+    "doorCommand" : 1,
+    "truckWeight" : 565,
+    "alarmCtrl    : 0
+}
+```
+
+### Additional Information 
+
+project name: Atmel Studio PRojects <br/>
+version: v0r1 <br/> 
+author: Pedro Vilela (pvilela)  
+date: 15/10/2019
---
+++ mqtt/mqtt.ino
@@ -0,0 +1,535 @@
+/**
+  @file: main.c
+  @class: main
+  @version: v0r1
+  @author: pvilela
+  @date: 19/10/2019
+  @brief: main class implementation.
+  This is the implementation of mqtt broker to interface with nodeMCU board.
+  @since  VER   DATE        COMMENTS
+  @since  v0r1  19/10/2019  Class creation
+ */
+
+/*****************************
+ *  NodeMCU pin definitions  * 
+ *****************************
+ *      D0          16       * 
+ *      D1          5        *
+ *      D2          4        *
+ *      D3          0        *
+ *      D4          2        *
+ *      D5          14       *
+ *      D6          12       *
+ *      D7          13       *
+ *      D8          15       *
+ *      D9          3        * 
+ *      D10         1        *
+ *****************************/
+
+/*********************************************************
+ * Includes.
+*********************************************************/
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+/*********************************************************
+ * Private definitions.
+*********************************************************/
+#define DOOR_PIN            13
+#define ALARM_PIN           12
+#define SCALE_PIN           A0
+#define BUZZER_PIN          14
+#define SENSOR_PIN          4
+
+#define SERIAL_BAUDRATE     115200
+
+#define WIFI_SSID           "WLL-Inatel"
+#define WIFI_PASSWORD       "inatelsemfio"   
+
+#define MQTT_SERVER         "broker.mqtt-dashboard.com"
+#define MQTT_PORT           1883
+#define MQTT_PUB_TOPIC      "icc/truck_report"
+#define MQTT_SUB_TOPIC      "icc/truck_command"
+#define MQTT_USERNAME       "mosquitto"
+#define MQTT_PASSWORD       "mosquitto"
+#define MQTT_CLIENT_ID      "phvilela87"
+
+#define BUZZER_ON()         digitalWrite(BUZZER_PIN, HIGH)
+#define BUZZER_OFF()        digitalWrite(BUZZER_PIN, LOW)
+
+/*********************************************************
+ * Private types.
+*********************************************************/
+WiFiClient wifiClient;
+PubSubClient client(MQTT_SERVER, MQTT_PORT, wifiClient);
+StaticJsonDocument<256> tx_doc;
+StaticJsonDocument<256> rx_doc;
+
+typedef enum enu_BuzzerLoopControl
+{
+  BUZZER_INIT = 0,
+  BUZZER_ENABLE,
+  BUZZER_ENABLE_VERIFY,
+  BUZZER_DISABLE,
+  BUZZER_IDLE,
+}tenu_BuzzerLoopControl;
+
+typedef struct tag_truckCtrl
+{
+  uint8_t                   ucAlarmStatus;
+  uint8_t                   ucDoorStatus;
+  int16_t                   iTruckWeight; 
+  uint8_t                   ucRxAlarmCtrl;
+  uint8_t                   ucRxDoorCommand;
+  int16_t                   iRxTruckWeight; 
+  uint32_t                  ulReferenceTime;
+  uint32_t                  ulCurrentTime;
+  uint32_t                  ulLastMessageArrived;
+  tenu_BuzzerLoopControl    enuBuzzerLoopControl;
+}ttag_truckCtrl;
+
+/*********************************************************
+ * Private variables.
+*********************************************************/
+int8_t status = WL_IDLE_STATUS;
+
+ttag_truckCtrl  tagTruckCtrl;
+
+/*********************************************************
+ * Private function prototypes.
+*********************************************************/
+void setup_wifi(void);
+void callback(char* topic, unsigned char* payload, unsigned int length);
+void system_init(void);
+void lock_door(void);
+void unlock_door(void);
+void activate_alarm(void);
+void deactivate_alarm(void);
+
+/*********************************************************
+ * Private functions.
+*********************************************************/
+/**
+  Main class set-up.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void setup(void) 
+{
+  pinMode(DOOR_PIN, OUTPUT);
+  pinMode(ALARM_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(SCALE_PIN, INPUT);
+  pinMode(SENSOR_PIN, INPUT_PULLUP);
+
+  digitalWrite(DOOR_PIN, LOW);
+  digitalWrite(ALARM_PIN, LOW);
+  digitalWrite(BUZZER_PIN, LOW);
+  
+  system_init();
+  unlock_door();
+  deactivate_alarm();
+  
+  Serial.begin(SERIAL_BAUDRATE);
+ 
+  delay(10);
+  
+  setup_wifi();
+  client.setServer( MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);
+
+  // Connecting to MQTT Broker
+  if(client.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) 
+  {
+    Serial.println("Connected to MQTT Broker!");
+    client.subscribe(MQTT_SUB_TOPIC);
+  }
+  else 
+  {
+    Serial.println("Connection to MQTT Broker has failed...");
+  }
+}
+
+/**
+  WiFi set-up.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void setup_wifi(void)
+{
+  Serial.print("\nConnecting to ");
+  Serial.println(WIFI_SSID);
+
+  // Connects to the WiFi network
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  // Wait until the connection has been confirmed before continuing
+  while(WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Outputs the IP Address of the ESP8266
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/**
+  Recconects to MQTT broker.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void reconnect(void)
+{
+  status = WiFi.status();
+  if( status != WL_CONNECTED)
+  {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to WiFi");
+  }
+
+  while(!client.connected())
+  {
+    Serial.println("Attempting MQTT connection...");
+    if(client.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD))
+    {
+      Serial.println("MQTT connected");
+      client.subscribe(MQTT_SUB_TOPIC);
+    }
+    else
+    {
+      Serial.println("Failed to connect, rc=");
+      Serial.println(client.state());
+      Serial.println("Trying again in 5 seconds...");
+      delay(5000);
+    }
+  }
+}
+
+/**
+  Initialises all global variables.
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void system_init(void)
+{
+  tagTruckCtrl.ucAlarmStatus = 0;
+  tagTruckCtrl.ucDoorStatus = 0;
+  tagTruckCtrl.iTruckWeight = 0;
+  tagTruckCtrl.ucRxAlarmCtrl = 0;
+  tagTruckCtrl.ucRxDoorCommand = 0;
+  tagTruckCtrl.iRxTruckWeight = 0; 
+  tagTruckCtrl.ulReferenceTime = 0;
+}
+
+/**
+  Sends message via MQQT protocol.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void send_message(void)
+{
+  char tx_buffer[256];
+
+  tagTruckCtrl.iTruckWeight = get_truckWeight();
+
+  tx_doc["truckWeight"] = tagTruckCtrl.iTruckWeight;
+  tx_doc["alarmStatus"] = tagTruckCtrl.ucAlarmStatus;
+  serializeJsonPretty(tx_doc, tx_buffer);
+
+  client.publish(MQTT_PUB_TOPIC, tx_buffer);
+
+  //Serial.println(tx_buffer);
+}
+
+/**
+  Locks the door.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void lock_door(void)
+{
+  if(!tagTruckCtrl.ucDoorStatus && !read_sensor() && !tagTruckCtrl.ucAlarmStatus) 
+  {
+    tagTruckCtrl.ucDoorStatus = 1;
+    digitalWrite(DOOR_PIN, HIGH);
+    Serial.println("Door has been locked!");
+  }
+  else if(tagTruckCtrl.ucDoorStatus && !tagTruckCtrl.ucAlarmStatus)  
+  {
+    Serial.println("Door is already locked!");
+  }
+  else if(!tagTruckCtrl.ucDoorStatus && read_sensor() && !tagTruckCtrl.ucAlarmStatus) 
+  { 
+    Serial.println("Sensor Failure!");
+  }
+  else 
+  { 
+    Serial.println("Operation not allowed!");
+  }
+}
+
+/**
+  Unlocks the door.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void unlock_door(void)
+{
+  if(tagTruckCtrl.ucDoorStatus && !tagTruckCtrl.ucAlarmStatus) 
+  {
+    tagTruckCtrl.ucDoorStatus = 0;
+    digitalWrite(DOOR_PIN, LOW);
+    Serial.println("Door has been unlocked!");
+  }
+  else if(!tagTruckCtrl.ucDoorStatus && !tagTruckCtrl.ucAlarmStatus) 
+  { 
+    Serial.println("Door is already unlocked!");
+  }
+  else
+  {
+     Serial.println("Operation not allowed!");
+  }
+}
+
+/**
+  gets truck weight.
+
+    @param  void
+    @return weight in kg.
+    @author pvilela
+    @date   20/10/2019
+*/
+int16_t get_truckWeight(void)
+{
+  return analogRead(SCALE_PIN);
+}
+
+/**
+  Checks door sensor state.
+
+    @param  void
+    @return 0 - if door is closed ; 1 - otherwise
+    @author pvilela
+    @date   20/10/2019
+*/
+uint8_t read_sensor(void)
+{
+  return digitalRead(SENSOR_PIN);
+}
+
+/**
+  Verifies system integrity.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void verify_system(void)
+{
+  if(tagTruckCtrl.ucDoorStatus && read_sensor())
+  {
+    if(!tagTruckCtrl.ucAlarmStatus) 
+    {
+      activate_alarm();
+      tagTruckCtrl.enuBuzzerLoopControl = BUZZER_ENABLE;
+    }
+  }
+}
+
+/**
+  Informs any system violation and sends alarm message via MQTT protocol.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void activate_alarm(void)
+{
+  if(!tagTruckCtrl.ucAlarmStatus)
+  {
+    tagTruckCtrl.ucAlarmStatus = 1;
+    send_message();
+    digitalWrite(ALARM_PIN, HIGH);
+    Serial.println("System Violation Alarm!");
+  }
+}
+
+void deactivate_alarm(void)
+{
+  if(tagTruckCtrl.ucAlarmStatus)
+  {
+    tagTruckCtrl.ucAlarmStatus = 0;
+    unlock_door();
+    tagTruckCtrl.enuBuzzerLoopControl = BUZZER_INIT;
+    digitalWrite(ALARM_PIN, LOW);
+    Serial.println("Alarm deactivated!");
+  }
+}
+
+/**
+  Buzzer process loop.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void buzzerCtrl_processLoop(void)
+{
+  switch (tagTruckCtrl.enuBuzzerLoopControl)
+  {
+    case BUZZER_INIT:
+      BUZZER_OFF();
+      tagTruckCtrl.enuBuzzerLoopControl = BUZZER_IDLE;
+      break;
+    case BUZZER_ENABLE:
+      BUZZER_ON();
+      tagTruckCtrl.enuBuzzerLoopControl = BUZZER_ENABLE_VERIFY;
+      tagTruckCtrl.ulReferenceTime = millis();
+      break;
+    case BUZZER_ENABLE_VERIFY:
+      if((millis() - tagTruckCtrl.ulReferenceTime) > 50)
+      {
+        BUZZER_OFF();
+        tagTruckCtrl.enuBuzzerLoopControl = BUZZER_DISABLE;
+        tagTruckCtrl.ulReferenceTime = millis();
+      }
+      break;
+    case BUZZER_DISABLE:
+      if((millis() - tagTruckCtrl.ulReferenceTime) > 200)
+      {
+        if(tagTruckCtrl.ucAlarmStatus)
+        {
+          tagTruckCtrl.enuBuzzerLoopControl = BUZZER_ENABLE;
+        }
+        else
+        {
+          tagTruckCtrl.enuBuzzerLoopControl = BUZZER_INIT;
+        }
+      }
+      break;
+    case BUZZER_IDLE:
+      break;
+    default:
+      break;
+  }
+}
+
+/**
+  Parsers received messages from callback.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void rx_message_parser(void)
+{
+  if(!tagTruckCtrl.ucRxDoorCommand && !tagTruckCtrl.ucRxAlarmCtrl) 
+  { 
+    unlock_door();
+  }
+  else if(tagTruckCtrl.ucRxDoorCommand && !tagTruckCtrl.ucRxAlarmCtrl) 
+  { 
+    lock_door();
+  }
+  else
+  {
+    Serial.println("Operation not allowed!");
+  }
+  
+  if(tagTruckCtrl.ucRxAlarmCtrl) 
+  { 
+    deactivate_alarm();
+  }
+}
+
+/**
+  Main process loop.
+
+    @param  void
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void loop(void) 
+{
+  if(!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  verify_system();
+  buzzerCtrl_processLoop();
+  
+  tagTruckCtrl.ulCurrentTime = millis();
+  if(tagTruckCtrl.ulCurrentTime - tagTruckCtrl.ulLastMessageArrived > 5000)
+  {
+    send_message();
+    tagTruckCtrl.ulLastMessageArrived = tagTruckCtrl.ulCurrentTime;
+  }
+}
+
+/**
+  MQTT callback function.
+
+    @param  topic       pointer to the subscribed topic
+            payload     pointer to payload
+            length      payload length in bytes
+    @return void
+    @author pvilela
+    @date   20/10/2019
+*/
+void callback(char* topic, unsigned char* payload, unsigned int length)
+{
+  DeserializationError err = deserializeJson(rx_doc, payload);
+
+  tagTruckCtrl.ucRxDoorCommand = rx_doc["doorCommand"];
+  tagTruckCtrl.iRxTruckWeight = rx_doc["truckWeight"];
+  tagTruckCtrl.ucRxAlarmCtrl = rx_doc["alarmCtrl"];
+
+  if(err)
+  {
+    //Serial.print("Error: ");
+    //Serial.println(err.c_str());
+    return;
+  }
+
+  rx_message_parser();
+
+//Serial.println(tagTruckCtrl.ucRxDoorCommand);
+  Serial.println(tagTruckCtrl.iRxTruckWeight);
+//Serial.println(tagTruckCtrl.ucRxAlarmCtrl);
+}
